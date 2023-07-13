01a137e5066ca5d175382cf59e3c99099590fce6
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-07-13 22:30:47 +0330

    Remove unneeded codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {
-  BrowserRouter,
-  Route,
-  RouterProvider,
-  Routes,
-  createBrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import LoginScreen from "./components/screens/LoginScreen";
 import HomeScreen from "./components/screens/HomeScreen";
@@ -13,21 +7,6 @@ import { CssBaseline, createTheme } from "@mui/material";
 import RegisterScreen from "./components/screens/RegisterScreen";
 import { SWRConfig } from "swr";
 import { swrGlobalConfig } from "./api/config";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomeScreen />,
-//   },
-//   {
-//     path: "/login",
-//     element: <LoginScreen />,
-//   },
-//   {
-//     path: "/register",
-//     element: <RegisterScreen />,
-//   },
-// ]);
 
 const theme = createTheme();
 
